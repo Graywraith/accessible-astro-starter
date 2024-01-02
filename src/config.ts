ada1c0d951ca07707e851d8b83a36bc3386e85c0
Fossil Oak <richard.noake@live.co.uk>
2024-01-02 13:49:24 +0000

    initial work on conversion to noake,me

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+/* Place any global data in this file. */
+/* You can import this data from anywhere in your site by using the `import` keyword. */
+
+export const SITE_TITLE = 'Noake, Richard';
+export const SITE_DESCRIPTION = 'The personal website for Richard Noake. My latest online presence and blog.  With information about the surname Noake, my interests and history.';
+export const SITE_VERSION = '1.00.05'
