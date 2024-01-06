2ccad23a171500eebfd4007073c58df9f65115e0
Fossil Oak <richard.noake@live.co.uk>
2024-01-06 11:49:51 +0000

    progress post display

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-
 import react from "@astrojs/react";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), tailwind({
     applyBaseStyles: false
-  }), compress(), react()]
+  }), compress(), react(), solidJs()]
 });
